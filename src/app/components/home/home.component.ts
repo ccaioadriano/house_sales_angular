c6c202b4541585725bc00c4d6e6bc16125c98a6b
Caio Adriano <caiobhadriano@gmail.com>
2023-12-09 15:39:22 -0300

    Usando dados de uma interface

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HousingLocation } from 'src/app/interfaces/HousingLocation';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+  housingLocation: HousingLocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `https://placehold.co/600x400`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
+
 
 }
