da8a32139a5f4534d5192b9d4e502897010cdb7c
Caio Adriano <caiobhadriano@gmail.com>
2023-12-10 23:36:08 -0300

    Criando a navegação para o component details

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
-
+import { HousingService } from './../../services/housing.service';
+import { HousingLocation } from './../../interfaces/HousingLocation';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.scss']
+  styleUrls: ['./details.component.scss'],
 })
 export class DetailsComponent {
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingLocationId = -1;
 
+  housingService: HousingService = inject(HousingService);
+  housingLocation: HousingLocation | undefined;
+
+
+  constructor() {
+    this.housingLocationId = Number(this.route.snapshot.params['id']);
+    this.housingLocation = this.housingService.getHousingLocationById(this.housingLocationId);
+  }
 }
